6b27e3ce45553109a9539d803f2df9c8c6ad3653
agustintrossero <agustintrossero@gmail.com>
2023-01-23 21:34:56 +0100

    creacion de componente Mensaje.jsx con children y tipo  para que itere con validaciones desde el formulario en el componente de NuevoPresupuesto, hasta aqui se implemento para que el tipo de dato que lee sea solo de tipo Number, sino es Number devuelve un mensaje de error con sus respectivos estilos, paso siguiente añadir un dato valido y  ver como lo acepta el resto de la aplicacion en otros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0)
   return(
   <div className="App">
-    <Header/>
+    <Header
+      presupuesto = {presupuesto}x
+      setPresupuesto = {setPresupuesto}
+    />
   </div>
   )
 
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,21 +1,41 @@
-import React from "react"
+import {useState} from "react"
+import Mensaje from "./Mensaje"
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState("")
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        //console.log("Enviando el formulario de presupuesto")
+        //console.log(Number(presupuesto))
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0) {
+            setMensaje("No es un presupuesto valido")
+        }else {
+            setMensaje("Es un presupuesto valido")
+        }
+    }
+
     return (
-        <div   className="contenedor-presupuesto contenedor sombra">
-            <form action="" className="formulario">
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form onSubmit={handlePresupuesto} action="" className="formulario">
                 <div className="campo">
                     <label htmlFor="">Definir Presupuesto</label>
                     <input
                     className="nuevo-presupuesto" 
                     type="text"
                     placeholder="Añade tu Presupuesto" 
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(e.target.value)}
                     />
                 </div>
                 <input type="submit" value="Añadir" />
+                {mensaje && <Mensaje tipo= "error">{mensaje}</Mensaje>}
             </form>
         </div>
     )
 }
 
 export default NuevoPresupuesto
+
+
