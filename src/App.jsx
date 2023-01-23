25263758d8b2823dcfe75e5513d8118e9e98b81a
agustintrossero <agustintrossero@gmail.com>
2023-01-23 16:28:43 +0100

    creacion de componentes Header y NuevoPresupuesto, se le asigno las clases yo tiene los estilos completos, paso siguiente definir el state del presupuesto para que estee disponible en toda la appp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
   return(
-    <h1>Hola</h1>
+  <div className="App">
+    <Header/>
+  </div>
   )
 
 }
